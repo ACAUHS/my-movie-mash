bbed8b3ad79eafcc97d7fd0d6730152106e36ef5
stevenning.203 <stevenning.203@gmail.com>
2022-12-20 20:48:49 -0800

    i fixed the requierd changes

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <Head>
             <title>Create Next App</title>
-            <meta name="description" content="Movies list!" />
+            <meta name="description" content="Movie List!" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
