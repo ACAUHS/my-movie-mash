547b2b55e519ea0cfee2446c23adc8d35e2333cb
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-22 20:13:23 -0500

    Fix sizing issue with buttons

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -6,7 +6,7 @@
 //make buttons hover text and bigger!
 
 export default function NavBar () {
-    const buttonStyle = ' rounded-full bg-sky-600 p-6 transition '
+    const buttonStyle = ' rounded-full bg-sky-600 p-6 transition shrink-0 min-h-min max-h-8'
     return (
         <div className="bg-sky-50 flex justify-between p-6">
             <header className="text-xl font-bold text-sky-600 ">My Movie Mash</header>
