d1e91cbdef332fd939de199aad2f9f23e01420ad
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-22 23:47:46 -0500

    Add placeholder to searchbar

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -5,15 +5,13 @@ const buttonStyle = 'group rounded-full text-white bg-sky-600 p-6 shrink-0 min-h
 
 /**
  * @todo 
- * make buttons hover text and bigger!
+ * 
  * 
  * viewport for steven is messed up (margin on top is messed look at discord for more details)
- * 
- * center the icons to the button and make it white
- * 
- * add transitionout style
- * 
+ *
  * search bar is messed up when adding text to it
+ * add placeholder text to search bar
+ * add button indicating search beside search bar
  * 
  * @returns Makes the navigation bar to the website.
  */
@@ -22,7 +20,9 @@ export default function NavBar () {
     return (
         <div className="bg-sky-50 flex justify-between p-6">
             <a href='../'><header className="text-xl font-bold text-sky-600 mt-2">My Movie Mash</header></a>
-            <div className="flex gap-6 justify-between">
+            
+            {/** navbar for medium screens and above */}
+            <div className="hidden md:flex gap-6 justify-between">
                 <a href='/list'>
                     <NavBarIcon icon={<HiBars3 size="25" />} text ='Your list' />
                 </a>
@@ -33,7 +33,23 @@ export default function NavBar () {
                     <NavBarIcon icon={<FiFilm size="25" />} text = 'Random movie'/>
                 </a>
             </div>
-            <input type="search" id="movie-search" className="bg-sky-100 border-2 border-sky-600 rounded-md max-h-10 pt-6 mt-1"/>
+
+            {/** mobile icon thing*/}
+            <div className="md:hidden flex items-center">
+                <button className="text-sky-600">
+                    <HiBars3 size="30" />
+                </button>
+            </div>
+
+            {/** mobile menu */}
+            <div className="md:hidden">
+                <a href='/list' className="block py-2 text-sm px-2 text-sky-600 hover:bg-sky-500 hover:text-white">Your list</a>
+                <a href='/random-show' className="block py-2 text-sm px-2 text-sky-600 hover:bg-sky-500 hover:text-white">Random show</a>
+                <a href='/random-movie' className="block py-2 text-sm px-2 text-sky-600 hover:bg-sky-500 hover:text-white">Random movie</a>
+            </div>
+
+            {/** search bar for medium screens+ */}
+            <input type="search" id="movie-search" placeholder="Search" className="hidden md:flex bg-sky-100 border-2 border-sky-600 rounded-md max-h-10 mt-1 shrink outline-none pl-3"/>
         </div>
     )
 }
